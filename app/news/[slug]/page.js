f455a742d6611e9bb8ff96f2bca2ebdff0617a2f
hamed <52428565+hamedmkm@users.noreply.github.com>
2025-01-12 20:13:27 +0330

    update slug

--- app/news/[slug]/page.js
+++ app/news/[slug]/page.js
@@ -1,25 +1,38 @@
-import { DUMMY_NEWS } from "@/dummy-news"
-import { notFound } from "next/navigation";
+import { DUMMY_NEWS } from '@/dummy-news';
 
+// استفاده از async برای دریافت داده‌ها
+export async function generateMetadata({ params }) {
+  // منتظر بمانید تا `params` بارگذاری شود
+  const { slug } = await params;
+  const newsItem = DUMMY_NEWS.find((item) => item.slug === slug);
 
-export default function NewsDetailPage({params}){
-    const newsSlug = params.slug
-    const newsItem = DUMMY_NEWS.find((newsItem)=> newsItem.slug === newsSlug);
+  if (!newsItem) {
+    return { title: 'News not found' }; // نمایش خطا در صورت عدم پیدا کردن
+  }
 
-    if(!newsItem) {
-        notFound();
-    }
-    return (
-        <article className="news-article">
-            <header>
-                <img src={`/images/news/${newsItem.image}`} alt={newsItem.title} />
-        <h1>{newsItem.title}</h1>
-        <time dateTime={newsItem.date}></time>
-        </header>
-
-  <p>{newsItem.content}</p>
-        </article>
-    )
+  return {
+    title: newsItem.title,
+    description: newsItem.content,
+  };
 }
 
+export default async function NewsDetailPage({ params }) {
+  // منتظر بمانید تا `params` بارگذاری شود
+  const { slug } = await params;
+  const newsItem = DUMMY_NEWS.find((item) => item.slug === slug);
 
+  if (!newsItem) {
+    return <div>News not found!</div>;
+  }
+
+  return (
+    <article className="news-article">
+      <header>
+        <img src={`/images/news/${newsItem.image}`} alt={newsItem.title} />
+        <h1>{newsItem.title}</h1>
+        <time dateTime={newsItem.date}>{newsItem.date}</time>
+      </header>
+      <p>{newsItem.content}</p>
+    </article>
+  );
+}
