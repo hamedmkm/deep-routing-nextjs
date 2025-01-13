5a626c6787abd5acc7ea4457e4a6468810567450
hamed <52428565+hamedmkm@users.noreply.github.com>
2025-01-13 17:54:57 +0330

    add async function for warning

--- app/archive/@archive/[year]/page.js
+++ app/archive/@archive/[year]/page.js
@@ -1,9 +1,12 @@
 import NewsList from "@/component/News-List/news-list";
 import { getNewsForYear } from "@/lib/news/news";
 
-export default  function FilteredNewsPage({ params }) {
-  const newsYear = params.year;
-  const news = getNewsForYear(newsYear);
+export default async function FilteredNewsPage({ params }) {
+  // مطمئن شوید params به درستی await شده است
+  const { year } = await params;
+
+  // دریافت داده‌ها برای سال مشخص‌شده
+  const news = getNewsForYear(year);
 
   return <NewsList news={news} />;
 }
